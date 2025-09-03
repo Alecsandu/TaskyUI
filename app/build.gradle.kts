e7b738581d36585ffa40dd47a8eac250573cb168
Alexandru-Cristian Sandu <alecsandu678@gmail.com>
2025-09-03 18:40:21 +0300

    [Misc] Format dependencies files
    Signed-off-by: Alexandru-Cristian Sandu <alecsandu678@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,10 +57,12 @@ dependencies {
 
     // Compose
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+
+    // Compose Activity
     implementation(libs.androidx.activity.compose)
 
     // Navigation
@@ -85,9 +87,9 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
 
     // ------------------------------ Debug --------------------------------------------- \\
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
